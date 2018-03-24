2438cb9ee67c41046b38a014486952ff65a929f6
3837162+carlsonsantana@users.noreply.github.com <carlsonsantana@gmail.com>
2018-03-24 15:10:48 -0300

    :wheelchair: Add HaTeMiLe for Browser

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,37 @@
 (function($){
-	$(document).ready(function(){
-		//Empty space. Fill this with your site specific JS code
-	});
+    $(document).ready(function() {
+        var configure = new hatemile.util.Configure(hatemile_configuration);
+        var htmlParser = new hatemile.util.html.vanilla
+                .VanillaHTMLDOMParser(document);
+        
+        var accessibleEvent = new hatemile.implementation
+                .AccessibleEventImplementation(htmlParser);
+        accessibleEvent.makeAccessibleAllClickEvents();
+        accessibleEvent.makeAccessibleAllDragandDropEvents();
+        accessibleEvent.makeAccessibleAllHoverEvents();
+        
+        var accessibleAssociation = new hatemile.implementation
+                .AccessibleAssociationImplementation(htmlParser, configure);
+        accessibleAssociation.associateAllDataCellsWithHeaderCells();
+        
+        var accessibleDisplay = new hatemile.implementation
+                .AccessibleDisplayScreenReaderImplementation(htmlParser,
+                        configure, window.navigator.userAgent);
+        accessibleDisplay.displayAllWAIARIAStates();
+        accessibleDisplay.displayAllLinksAttributes();
+        accessibleDisplay.displayAllAlternativeTextImages();
+        accessibleDisplay.displayAllTitles();
+        accessibleDisplay.displayAllShortcuts();
+        
+        var accessibleNavigation = new hatemile.implementation
+                .AccessibleNavigationImplementation(htmlParser, configure,
+                        hatemile_configuration_skippers);
+        accessibleNavigation.provideNavigationByAllHeadings();
+        accessibleNavigation.provideNavigationToAllLongDescriptions();
+        accessibleNavigation.provideNavigationByAllSkippers();
+        
+        accessibleDisplay.displayAllShortcuts();
+        accessibleDisplay.displayAllLanguages();
+        accessibleDisplay.displayAllRoles();
+    });
 })(jQuery);
